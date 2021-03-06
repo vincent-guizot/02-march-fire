767c7b9a6f785c5bc08ccd6174bc198b96fd244f
Vincent Guizot <vincentguizot128@gmail.com>
2021-03-06 11:52:20 +0700

    algoritma dan pseudocode

---
+++ index.js
@@ -0,0 +1,117 @@
+// ALGORITMA -> PSEUDOCODE -> CODING
+
+// How to make Indomie
+
+/**
+ * 1. Ambil panci
+ * 2. Isi panci dengan air
+ * 3. Nyalakan kompor
+ * 4. Taro panci di atas kompor
+ * 5. Ambil indomie, lalu buka bungkus dan bumbunya
+ * 6. Tuang bumbu ke piring
+ * 7. Masukkan mie ke panci
+ * 8. Jika sudah matang, tiriskan mie 
+ * 9. Masukkan mie ke piring, campur bumbu
+ * 10. Lalu sajikan
+ */
+
+ // Hitung luas persegi panjang
+
+ // Algoritma
+ /**
+  * 1. Tentukan panjang
+  * 2. TEntukan lebar
+  * 3. Tentukan rumus
+  * 4. RUmus panjang kali lebar
+  * 5. Proses rumus
+  * 6. Tampilkan hasil
+  */
+
+  // PSEUDOCODE
+  /**
+   * STORE panjang WITH 5
+   * STORE lebar WITH 3
+   * 
+   * SET luas WITH panjang TIMES lebar
+   * DISPLAY luas
+   */
+
+   // Hitung luas lingkaran
+
+   // Algoritma
+   /**
+    * 1. TEntukan jari-jari
+    * 2. Tentukan nilai phi
+    * 3. Tentukan rumus
+    * 4. Rumus : phi * r * r
+    * 5. Tampilkan hasil 
+    */
+
+    // PSEUDOCODE
+    /**
+     * STORE jari-jari WITH ANY NUMBER
+     * SET phi WITH 3.14
+     * 
+     * SET luasLingkaran WITH phi TIMES jari-jari TIMES jari-jari
+     * DISPLAY luasLingkaran 
+     */
+
+     // Cek apakah angka genap atau ganjil
+
+     // Algoritma
+     /**
+      * 1. Buat variable angka
+      * 2. Cek angka, jika angka habis dibagi 2 maka genap
+      * 3. Cek angka, jika angka tidak habis dibagi 2 maka ganjil
+      * 4. Tampilkan hasil
+      */
+
+    //   +,=,*,/,%
+
+    //   7/3 = 2 sisa 1
+    //   7%3 = 1
+
+    //PSEUDOCODE
+    /**
+     * STORE angka WITH ANY NUMBER
+     * 
+     * IF angka MOD 2 EQUAL 0
+     *  DISPLAY "Angka Genap"
+     * ELSE IF angka MOD 2 EQUAL 1
+     *  DISPLAY "Angka Ganjil"
+     * END IF
+     */
+
+     // Cek angka, apakah kelipatan 3 atau kelipatan 2 atau kelipatan 2 dan 3
+
+     // ALGORITMA
+     /**
+      * 1. Tentukan angka
+      * 2. Cek angka :
+      *     2.1. Jika habis dibagi 2 maka tampilkan kelipatan 2
+      *     2.2. Jika habis dibagi 3 maka tampilkan kelipatan 3
+      *     2.3. Jika habis dibagi 2 dan 3 maka tampilkan kelipatan 2 dan 3
+      *     2.4. Selain itu, maka tampilkan "Kelipatan Lain"
+      */
+
+      // PSEUDOCODE 
+    /**
+     * STORE angka WITH ANY NUMBER
+     * 
+     * IF angka MOD 2 EQUAL 0
+     *  DISPLAY "Kelipatan 2"
+     * ELSE IF angka MOD 3 EQUAL 0
+     *  DISPLAY "Kelipatan 3"
+     * ELSE IF angka MOD 2 EQUAL 0 AND angka MOD 3 EQUAL 0
+     *  DISPLAY "Kelipatan 2 dan 3"
+     * ELSE 
+     *  DISPLAY "Kelipatan Lain"
+     * END IF
+     */
+
+    //  2 kelipatan 2
+    //  3 kelipatan 3
+    //  6 kelipatan 2 dan 3
+    //  8 kleipatan 2
+    //  9 kelipatan 3
+    //  12 kelipatn 2 dan 3
